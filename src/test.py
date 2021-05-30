8aa0707bd4ed01cdc297465a5a22ea44e0e90d2b
unknown <melvinchia623600@gmail.com>
2021-05-30 23:20:21 +0800

    log updated

--- src/test.py
+++
@@ -1,7 +0,0 @@
-from youtube_scraping_api import YoutubeAPI
-from youtube_scraping_api.filter import SearchFilter
-api = YoutubeAPI()
-
-
-for i in api.playlist('PL7VmhWGNRxKgtwHFgDMCnutcmiafoP1c4'):
-	print(i)
